a05548cef78c14c5ab82b60b98344cf77a736f3c
amritha2510 <amritha2510@gmail.com>
2024-11-10 18:09:49 +0800

    updates to popularity based filtering

--- recSys/popularityBased.py
+++ recSys/popularityBased.py
@@ -6,48 +6,58 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 import pymongo
+from supabase import create_client
+from functions import initialising_supabase, load_product_data
 ### Targeted at new customers, built on demographic filtering to resolve the cold start problem
 
-# Loading the dataset
-df = pd.read_csv('newData/synthetic_v2.csv')
-df['Order Date'] = pd.to_datetime(df['Order Date'])
+def load_order_data():
+    supabase = initialising_supabase()
+    # Load data from the flipkart_cleaned table in supabase
+    order_data = pd.DataFrame(supabase.table('synthetic_v2').select('*').execute().data)
 
-catalog = pd.read_csv('newData/flipkart_cleaned.csv')
-catalog.rename(columns={'uniq_id': 'Product ID'}, inplace=True)
+    print("Successfully loaded order from Supabase")
+ 
+    return order_data
+
+def get_trending_products():
+    # supabase = initialising_supabase()
+    # Fetch the catalogue & order data from Supabase
+    catalogue = load_product_data()
+    orderdata = load_order_data()   
+    # Filter for purchases within the past year
+    one_year_ago = datetime.now() - timedelta(days=365)
+    recent_purchases = orderdata[pd.to_datetime(orderdata['Order Date']) >= one_year_ago]
+    # Identify trending products by sale volume
+    popular_products = recent_purchases.groupby('uniq_id')['User rating for the product'].count().reset_index()
+    top_5_most_popular = popular_products.sort_values('User rating for the product', ascending=False).head()
+    detailed_top_products = pd.merge(top_5_most_popular, catalogue, on='Product ID', how='left')
+    detailed_top_products = detailed_top_products[['Product ID','product_name', 'discounted_price', 'description', 'User rating for the product']]
+    return detailed_top_products
+
+# # finding the product name and saving it into the top_5_most_popular dataframe
+# detailed_top_products = pd.merge(top_5_most_popular, catalog, on='Product ID', how='left')
+# detailed_top_products = detailed_top_products[['Product ID','product_name', 'discounted_price', 'description', 'User rating for the product']]
+
+# #print(detailed_top_products)
+
+# # saving to dataframe for easy retrieval
+# load_dotenv()
+# MONGODB_URI = os.getenv("MONGODB_URI")
+# FLIPKART = os.getenv("FLIPKART")
 
 
-# Get purchases within the past year
-one_year_ago = datetime.now() - timedelta(days=365)
-recent_purchases = df[df['Order Date'] >= one_year_ago]
+# client = pymongo.MongoClient(MONGODB_URI)
+# mydb = client[FLIPKART]
+# top5 = mydb.Top5Products
 
-# Getting trending products (by sale volume)
-popular_products = pd.DataFrame(recent_purchases.groupby('Product ID')['User rating for the product'].count())
-top_5_most_popular = popular_products.sort_values('User rating for the product', ascending=False).head()
-
-# finding the product name and saving it into the top_5_most_popular dataframe
-detailed_top_products = pd.merge(top_5_most_popular, catalog, on='Product ID', how='left')
-detailed_top_products = detailed_top_products[['Product ID','product_name', 'discounted_price', 'description', 'User rating for the product']]
-
-#print(detailed_top_products)
-
-# saving to dataframe for easy retrieval
-load_dotenv()
-MONGODB_URI = os.getenv("MONGODB_URI")
-FLIPKART = os.getenv("FLIPKART")
-
-
-client = pymongo.MongoClient(MONGODB_URI)
-mydb = client[FLIPKART]
-top5 = mydb.Top5Products
-
-# saving the top 5 most popular products to the database
-for index, row in detailed_top_products.iterrows():
-    document = {
-        "Product ID": row['Product ID'],
-        "product_name": row['product_name'],
-        "discounted_price": row['discounted_price'],
-        "description": row['description'],
-        "User rating for the product": row['User rating for the product']
-    }
-    #print(document)
-    top5.insert_one(document)
+# # saving the top 5 most popular products to the database
+# for index, row in detailed_top_products.iterrows():
+#     document = {
+#         "Product ID": row['Product ID'],
+#         "product_name": row['product_name'],
+#         "discounted_price": row['discounted_price'],
+#         "description": row['description'],
+#         "User rating for the product": row['User rating for the product']
+#     }
+#     #print(document)
+#     top5.insert_one(document)
